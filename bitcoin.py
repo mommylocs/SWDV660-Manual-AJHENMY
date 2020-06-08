133f52ba0a3a45d7bf2a924ad45d2902347a407e
AJ Henmy <46701390+mommylocs@users.noreply.github.com>
2020-06-07 21:24:36 -0400

    Added files to Jenkins Job

---
+++ bitcoin.py
@@ -0,0 +1,11 @@
+# Please write your bitcoin_converter program in this file.
+
+print ('As of 06/07/20 at 09:20 pm bitcoin is currently trading at $9,751 US Dollars per bitcoin.')
+usdollars = int(input('Enter the number of bitcoins you want to convert:'))
+bitcoinamt = usdollars * 9751
+print ('That is a total of $',bitcoinamt, 'US dollars')
+    
+
+
+
+
